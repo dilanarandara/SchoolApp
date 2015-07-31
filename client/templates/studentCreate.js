53f68dd82352f07ac5b0c41b7601b79f79acbff0
Dilan Arandara <dilan.madu@gmail.com>
2015-08-01 00:47:31 +0530

    Form Validation added.

--- client/templates/studentCreate.js
+++ client/templates/studentCreate.js
@@ -1,10 +1,10 @@
 Template.studentCreate.events({
 	'submit form': function(event) {
 		event.preventDefault();
-		var firstName = $('#firstName').val();
-		var lastName = $('#lastName').val();
-		var email = $('#email').val();
-		var age = $('#age').val();
+		var firstName = $('[name="firstName"]').val();
+		var lastName = $('[name="lastName"]').val();
+		var email = $('[name="email"]').val();
+		var age = $('[name="age"]').val();
 		var student = {
 			firstName: firstName,
 			lastName: lastName,
@@ -12,9 +12,47 @@ Template.studentCreate.events({
 			age: age
 		};
 		Students.insert(student);
-		$('#firstName').val('');
-		$('#lastName').val('');
-		$('#email').val('');
-		$('#age').val('');
+		$('[name="firstName"]').val('');
+		$('[name="lastName"]').val('');
+		$('[name="email"]').val('');
+		$('[name="age"]').val('');
 	}
+});
+
+Template.studentCreate.onRendered(function() {
+	$('.form-horizontal').validate({
+		rules : {
+			firstName : {
+				required : true,
+				minlength : 2
+			},
+			lastName : {
+				required : true,
+				minlength : 2
+			},
+			email : {
+				required : true
+			},
+			age : {
+				required : true,
+				maxlength : 2
+			}
+		},
+		messages: {
+			firstName: {
+				required: 'First Name is required.',
+				minlength : 'First name should have at least {0} characters.'
+			},
+			lastName: {
+				required: 'Last Name is required.',
+				minlength : 'Last name should have at least {0} characters.'
+			},
+			email: {
+				required: 'Email is required.'
+			},
+			age: {
+				required: 'Age is required.'
+			}
+		}
+	});
 });
